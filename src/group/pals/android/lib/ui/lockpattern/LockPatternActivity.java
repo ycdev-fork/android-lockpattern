59f05e827211ac410517624cfc9275e64178174c
Hai Bison <Hai Bison@localhost>
2012-03-09 13:46:38 +0700

    - remove unused imports;

--- src/group/pals/android/lib/ui/lockpattern/LockPatternActivity.java
+++ src/group/pals/android/lib/ui/lockpattern/LockPatternActivity.java
@@ -16,19 +16,18 @@
 
 package group.pals.android.lib.ui.lockpattern;
 
-import java.util.List;
-
 import group.pals.android.lib.ui.lockpattern.widget.LockPatternUtils;
 import group.pals.android.lib.ui.lockpattern.widget.LockPatternView;
 import group.pals.android.lib.ui.lockpattern.widget.LockPatternView.Cell;
 import group.pals.android.lib.ui.lockpattern.widget.LockPatternView.DisplayMode;
+
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
