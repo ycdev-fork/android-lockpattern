e2c95f547d4525e23a72b6901f5bda16b3dfb4ed
hai bison <hai bison@localhost>
2013-05-28 00:12:09 +0700

    Demo:  - Add command `About`;  - Set version to v10 (1.7);

--- demo/src/group/pals/android/lib/ui/lockpattern/demo/CommandsPrefsHelper.java
+++ demo/src/group/pals/android/lib/ui/lockpattern/demo/CommandsPrefsHelper.java
@@ -18,12 +18,18 @@ package group.pals.android.lib.ui.lockpattern.demo;
 
 import group.pals.android.lib.ui.lockpattern.LockPatternActivity;
 import group.pals.android.lib.ui.lockpattern.prefs.Prefs;
+import group.pals.android.lib.ui.lockpattern.util.UI;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.Preference;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 
 /**
  * Helper class for {@link PrefsActivity} and {@link PrefsActivity_v11}.
@@ -37,7 +43,7 @@ public class CommandsPrefsHelper {
     public static final int REQ_ENTER_PATTERN = 1;
 
     /**
-     * Initializes preferences.
+     * Initializes handler for commands.
      * 
      * @param activity
      *            the activity.
@@ -92,6 +98,66 @@ public class CommandsPrefsHelper {
                                                 getThemeForLockPatternActivity(activity));
                                 activity.startActivityForResult(intentActivity,
                                         REQ_ENTER_PATTERN);
+
+                                return true;
+                            }// onPreferenceClick()
+                        });
+
+        /*
+         * COMMAND "ABOUT..."
+         */
+        holder.findPreference(activity.getString(R.string.pkey_about))
+                .setOnPreferenceClickListener(
+                        new Preference.OnPreferenceClickListener() {
+
+                            @Override
+                            public boolean onPreferenceClick(
+                                    Preference preference) {
+                                /*
+                                 * Build the dialog.
+                                 */
+                                Dialog dialog = new Dialog(activity, UI
+                                        .resolveAttribute(activity,
+                                                R.attr.alp_theme_dialog));
+                                dialog.setTitle(R.string.about);
+                                dialog.requestWindowFeature(Window.FEATURE_LEFT_ICON);
+                                dialog.setCanceledOnTouchOutside(true);
+
+                                dialog.setContentView(R.layout.dialog_about);
+
+                                dialog.setFeatureDrawableResource(
+                                        Window.FEATURE_LEFT_ICON,
+                                        android.R.drawable.ic_dialog_info);
+
+                                /*
+                                 * Set text...
+                                 */
+                                TextView textInfo = (TextView) dialog
+                                        .findViewById(R.id.textview_info);
+                                textInfo.setText(activity.getString(
+                                        R.string.pmsg_about_info,
+                                        activity.getString(R.string.alp_lib_name),
+                                        activity.getString(R.string.alp_lib_version_name)));
+                                textInfo.setOnClickListener(new View.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(View v) {
+                                        try {
+                                            activity.startActivity(new Intent(
+                                                    Intent.ACTION_VIEW,
+                                                    Uri.parse("http://www.haibison.com")));
+                                        } catch (Throwable t) {
+                                            /*
+                                             * Ignore it.
+                                             */
+                                        }
+                                    }// onClick()
+                                });
+
+                                /*
+                                 * Show the dialog.
+                                 */
+                                dialog.show();
 
                                 return true;
                             }// onPreferenceClick()
