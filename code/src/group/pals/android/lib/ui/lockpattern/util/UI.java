bfca0971a7e86f801ec5e71bbecaa43f3caca991
hai bison <hai bison@localhost>
2014-01-29 05:24:58 +0700

    Add some helper methods to UI.

--- code/src/group/pals/android/lib/ui/lockpattern/util/UI.java
+++ code/src/group/pals/android/lib/ui/lockpattern/util/UI.java
@@ -201,10 +201,28 @@ public class UI {
      *         {@code 0} if not found.
      */
     public static int resolveAttribute(Context context, int resId) {
+        return resolveAttribute(context, resId, 0);
+    }// resolveAttribute()
+
+    /**
+     * Convenient method for {@link Context#getTheme()} and
+     * {@link Resources.Theme#resolveAttribute(int, TypedValue, boolean)}.
+     * 
+     * @param context
+     *            the context.
+     * @param resId
+     *            The resource identifier of the desired theme attribute.
+     * @param defaultValue
+     *            the default value if cannot resolve {@code resId}.
+     * @return the resource ID that {@link TypedValue#resourceId} points to, or
+     *         {@code defaultValue} if not found.
+     */
+    public static int resolveAttribute(Context context, int resId,
+            int defaultValue) {
         TypedValue typedValue = new TypedValue();
         if (context.getTheme().resolveAttribute(resId, typedValue, true))
             return typedValue.resourceId;
-        return 0;
+        return defaultValue;
     }// resolveAttribute()
 
 }
